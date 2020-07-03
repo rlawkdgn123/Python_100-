27c4153d3e71188c8bc54ccdf7edb2b0e11f6357
rlawkdgn123 <63436456+rlawkdgn123@users.noreply.github.com>
2020-07-03 13:12:53 +0900

    2020/07/03

--- Untitled.py
+++ Untitled.py
@@ -1,107 +1,119 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[34]:
+# In[9]:
 
 
-import turtle as t
-t.shape('turtle')
-t.fd(200)
-t.fd(200)
-t.left(90)
-t.fd(200)
-t.left(90)
-t.fd(200)
-t.fd(200)
-t.left(90)
-t.fd(200)
-t.exitonclick()
+a=[[1,1,0,1,0],
+  [1,0,1,0]]
+tot, totsu = 0, 0
+for i in a:
+    for j in i:
+        tot += j
+    totsu = totsu + len(i)
+print(totsu,tot)
 
 
-# In[37]:
+# In[12]:
 
 
-import turtle as t 
-t.shape('turtle')
-t.fd(200)
-t.left(120)
-t.fd(200)
-t.left(120)
-t.fd(200)
-t.left(120)
-t.exitonclick()
+i,hap =1,0
+while i<=6:
+    hap+=i
+    i+=2
+print(f"i={i},hap={hap}")
+
+
+# In[38]:
+
+
+str='Sinagong'
+n=len(str)
+st=list()
+for k in range(n):
+    st.append(str[k])
+for k in range(n-1,-1,-1):
+    print(st[k],end='')
 
 
 # In[39]:
 
 
-from turtle import *
-color('red', 'yellow')
-begin_fill()
-while True:
-    forward(200)
-    left(170)
-    if abs(pos()) < 1:
-        break
-end_fill()
-done()
+hap = 0
+for i in range(1,11):
+    hap += i
+print(i,hap)
 
 
-# In[45]:
+# In[41]:
 
 
-import turtle as t 
-color('blue', 'yellow')
-begin_fill()
-t.shape('turtle')
-t.fd(200)
-t.left(120)
-t.fd(200)
-t.left(120)
-t.fd(200)
-t.left(120)
-end_fill()
-t.exitonclick()
+hap1=10+10%4-10%9
+hap2=10*10%4-10%9+5
+print("%d, %d"%(hap1,hap2))
 
 
-# In[185]:
+# In[50]:
 
 
-import turtle as t 
-t.shape('turtle')
-t.left(90)
-t.fd(200)
-t.left(170)
-t.fd(450)
-t.left(150)
-t.fd(400)
-t.left(130)
-t.fd(330)
-t.left(130)
-t.fd(400)
-t.left(153.5)
-t.fd(445)
+a,b=1,1
+y=a+b
+n=int(input())
+for k in range(3,n+1):
+    c=a+b
+    y=y+c
+    a=b
+    b=c
+print(y)
 
 
-t.exitonclick()
+# In[7]:
 
 
-# In[57]:
+a= "what's this?"
+print("%-10.4s" % a)
+print("%10.4s" % a)
 
 
-import turtle as t 
-t.shape('turtle')
-t.color('blue', 'yellow')
-t.begin_fill()
+# In[2]:
 
-n=int(input('몇각형?'))
-for i in range(n):
-    for i in range(n):
-        t.fd(100)
-        t.left(360/n)
-        t.fd(30)
-    t.right(180/n)
-    
-t.end_fill()
-t.exitonclick()
+
+a=sum=0
+while a < 10:
+    a+=1
+    if a%2==1:
+        continue
+    sum+=a
+print(sum)
+
+
+# In[3]:
+
+
+a,b=2,3
+c=a&b
+print(c)
+
+
+# In[4]:
+
+
+a,c=32,-3
+b=a<<2
+a>>=3
+c=c<<2
+print(a,b,c)
+
+
+# In[6]:
+
+
+def change():
+    global i,j
+    temp = i
+    i = j
+    j = temp
+i,j=10,20
+change()
+print(f"i={i},j={j}")
 
